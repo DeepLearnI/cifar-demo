e23532cc31c0c128d060730a209434774eb46662
Joseph Palermo <jp@deeplearni.ng>
2019-09-18 15:18:17 -0400

    cleanup

--- driver.py
+++ driver.py
@@ -1,14 +1,10 @@
+import foundations
 import tensorflow.keras
-from tensorflow.keras.datasets import cifar10
 from sklearn.model_selection import train_test_split
 from model import DepthwiseSeparableConvNet
-from datagen import datagen
-import foundations
-import pprint
-import os
 
 # load and preprocess data
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.cifar10.load_data()
 x_validation, x_test, y_validation, y_test = train_test_split(x_test, y_test, test_size=0.5)
 y_train = tensorflow.keras.utils.to_categorical(y_train, num_classes=10)
 y_test = tensorflow.keras.utils.to_categorical(y_test, num_classes=10)
@@ -17,9 +13,11 @@ x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
+# define hyperparameters
+hyperparameters = foundations.load_parameters()
+
 # train
 input_shape = x_train.shape[1:]
-hyperparameters = foundations.load_parameters()
 model = DepthwiseSeparableConvNet(input_shape, hyperparameters, num_classes=10)
 model.model.summary()
 model.train(x_train, y_train, x_test, y_test)
--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -34,16 +34,16 @@ def sample_hyperparameters(hyperparameter_ranges):
     return hyperparameters
 
 
-hyperparameter_ranges = {'num_epochs': 1,
+hyperparameter_ranges = {'num_epochs': 5,
                          'batch_size': 64,
-                         'learning_rate': 0.0001,
+                         'learning_rate': 0.001,
                          'depthwise_separable_blocks': [{'depthwise_conv_stride': 2, 'pointwise_conv_output_filters': 6},
                                                   {'depthwise_conv_stride': 2, 'pointwise_conv_output_filters': 12}],
                          'dense_blocks': [{'size': SearchSpace(64, 256, int),
                                            'dropout_rate': SearchSpace(0.1, 0.5, float)}],
                          'decay': 1e-6}
 
-num_jobs = 5
+num_jobs = 1
 for _ in range(num_jobs):
     hyperparameters = sample_hyperparameters(hyperparameter_ranges)
     foundations.submit(scheduler_config='scheduler', job_dir='.', command='driver.py', params=hyperparameters, stream_job_logs=True)
