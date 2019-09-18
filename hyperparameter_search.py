d4a6c2f01cab8a46ec14a862652301e941764acb
Joseph Palermo <jp@deeplearni.ng>
2019-09-18 14:43:55 -0400

    cleanup hyperparameter search

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -1,7 +1,37 @@
-from utils import SearchSpace, sample_hyperparameters
 import os
 os.environ['FOUNDATIONS_COMMAND_LINE'] = 'True'
 import foundations
+import numpy as np
+import copy
+
+
+class SearchSpace:
+
+    def __init__(self, min, max, type):
+        self.min = min
+        self.max = max
+        self.type = type
+
+    def sample(self):
+        if self.type == int:
+            return np.random.randint(self.min, self.max)
+        elif self.type == float:
+            return round(np.random.uniform(self.min, self.max), 2)
+
+
+def sample_hyperparameters(hyperparameter_ranges):
+    hyperparameters = copy.deepcopy(hyperparameter_ranges)
+    for hparam in hyperparameter_ranges:
+        if isinstance(hyperparameter_ranges[hparam], SearchSpace):
+            search_space = hyperparameter_ranges[hparam]
+            hyperparameters[hparam] = search_space.sample()
+        elif isinstance(hyperparameter_ranges[hparam], list):
+            for i, block in enumerate(hyperparameter_ranges[hparam]):
+                for block_hparam in block:
+                    if isinstance(block[block_hparam], SearchSpace):
+                        search_space = block[block_hparam]
+                        hyperparameters[hparam][i][block_hparam] = search_space.sample()
+    return hyperparameters
 
 
 hyperparameter_ranges = {'num_epochs': 1,
